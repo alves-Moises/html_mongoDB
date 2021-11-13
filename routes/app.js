1cf10efb59d7e1138f55ed972433a6dbf2e89250
alves-Moises <moisesinho_ra@hotmail.com>
2021-11-13 19:12:49 -0300

    new router post and get

--- routes/app.js
+++ routes/app.js
@@ -9,4 +9,16 @@ router.get('/message', function (req, res, next) {
     res.render('node', {message: 'Olá nova rota de mensagem.'});
 });
 
+
+router.post('/message', function (req, res, next) {
+    var messageVar = req.body.messageBody;
+    res.redirect('/message/' + messageVar);
+})
+
+// " : " indica que esta parte da URL é alguma variavel com dad
+router.get('/message:msgParam', function (req, res, next) { 
+    res.render('node', {message: req.params.msgParam});
+})
+
+
 module.exports = router;
